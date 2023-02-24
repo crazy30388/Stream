bfc328e69bde4e14c00c278f8933e40a7a70fdbf
crazy <crazy303@mail.ru>
2023-02-24 12:14:43 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+        List<T> items = stream.sorted(order).collect(Collectors.toList());
+        if (!items.isEmpty()) {
+            minMaxConsumer.accept(items.get(0), items.get(items.size() - 1));
+
+        } else {
+            minMaxConsumer.accept(null, null);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Stream<Integer> stream = new ArrayList<>(Arrays.asList(1, 2, 3, 5, 8, 13, 21)).stream();
+
+        findMinMax(
+                stream,
+                (x, y) -> x.compareTo(y),
+                (x, y) -> System.out.println(String.format("min: %s, max: %s", x, y))
+        );
+
+        stream.close();
+    }
+    public static  void findCountEvenNums(List<Integer> list) {
+        Stream<Integer> stream = list.stream();
+        Predicate<Integer> predicate;
+        predicate = (n) -> (n % 2) == 0;
+        Stream<Integer> resStream = stream.filter(predicate);
+        System.out.println("Количество четных чисел: " + resStream.count());
     }
 }
+
